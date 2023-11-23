95676d7909e4adb44d3dc8c672a7a3db788853c0
Astro064 <thomas135969@brisbanegrammar.com>
2023-11-23 16:11:03 +1000

    Mechanisms
    Mechanisms work, Drive does not

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterstageTeleOpMechOnly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterstageTeleOpMechOnly.java
@@ -1,14 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "Diff Swerve TeleOp", group = "TeleOp")
-public class TeleOp extends OpMode {
-    Robot robot;
+@TeleOp(name = "CenterstageTeleOpMechOnly (Blocks to Java)")
+
+public class CenterstageTeleOpMechOnly extends LinearOpMode {
+
     private DcMotor EncoderA;
     private DcMotor EncoderB;
     private DcMotor M1;
@@ -25,14 +27,27 @@ public class TeleOp extends OpMode {
     double Diff1Rest;
     double Diff2Rest;
 
-    //deadband for joysticks
-    public double DEADBAND_MAG = 0.1;
-    public Vector2d DEADBAND_VEC = new Vector2d(DEADBAND_MAG, DEADBAND_MAG);
+    /**
+     * Describe this function...
+     */
+    private void Climb() {
+        if (gamepad2.dpad_down) {
+            EncoderA.setPower(-1);
+            EncoderB.setPower(1);
+        } else if (gamepad2.dpad_up) {
+            EncoderA.setPower(1);
+            EncoderB.setPower(-1);
+        } else {
+            EncoderA.setPower(0);
+            EncoderB.setPower(0);
+        }
+    }
 
-    public boolean willResetIMU = true;
-
-    public void init() {
-        robot = new Robot(this, false);
+    /**
+     * This function is executed when this OpMode is selected from the Driver Station.
+     */
+    @Override
+    public void runOpMode() {
         int MaxVelocity;
         int C1;
         int C2;
@@ -50,76 +65,29 @@ public class TeleOp extends OpMode {
         Lift1 = hardwareMap.get(DcMotor.class, "Lift1");
         Lift2 = hardwareMap.get(DcMotor.class, "Lift2");
         IntakeR = hardwareMap.get(CRServo.class, "IntakeR");
-    }
 
-    //allows driver to indicate that the IMU should not be reset
-    //used when starting TeleOp after auto or if program crashes in the middle of match
-    //relevant because of field-centric controls
-    public void init_loop() {
-        if (gamepad1.y) {
-            willResetIMU = false;
-        }
-    }
-    public void start () {
-        if (willResetIMU) robot.initIMU();
+        // Put initialization blocks here.
+        waitForStart();
         LiftAndDiffSetup();
         M1.setDirection(DcMotor.Direction.REVERSE);
         EncoderA.setDirection(DcMotor.Direction.REVERSE);
         R2a.setDirection(DcMotor.Direction.REVERSE);
         EncoderB.setDirection(DcMotor.Direction.REVERSE);
-    }
-
-
-    public void loop() {
-        Vector2d joystick1 = new Vector2d(gamepad1.left_stick_x, -gamepad1.left_stick_y); //LEFT joystick
-        Vector2d joystick2 = new Vector2d(gamepad1.right_stick_x, -gamepad2.right_stick_y); //RIGHT joystick
-
-        robot.driveController.updateUsingJoysticks(checkDeadband(joystick1), checkDeadband(joystick2));
-
-
-        //uncomment for live tuning of ROT_ADVANTAGE constant
-        if (gamepad1.b) {
-          robot.driveController.moduleRight.ROT_ADVANTAGE += 0.01;
-        robot.driveController.moduleLeft.ROT_ADVANTAGE += 0.01;
-        }
-        if (gamepad1.x) {
-          robot.driveController.moduleRight.ROT_ADVANTAGE -= 0.01;
-            robot.driveController.moduleLeft.ROT_ADVANTAGE -= 0.01;
-        }
-        LiftAndDiffLoop();
-        Climb();
-        telemetry.addData("ROT_ADVANTAGE: ", robot.driveController.moduleLeft.ROT_ADVANTAGE);
-        telemetry.addData("TICKS_PER_MODULE_REV: ", robot.driveController.moduleLeft.TICKS_PER_MODULE_REV);
-
-
-        //to confirm that joysticks are operating properly
-        telemetry.addData("Joystick 1", joystick1);
-        telemetry.addData("Joystick 2", joystick2);
-
-        telemetry.update();
-
-    }
-
-    //returns zero vector if joystick is within deadband
-    public Vector2d checkDeadband(Vector2d joystick) {
-        if (Math.abs(joystick.getX()) > DEADBAND_VEC.getX() || Math.abs(joystick.getY()) > DEADBAND_VEC.getY()) {
-            return joystick;
-        }
-        return Vector2d.ZERO;
-    }
-
-    private void Climb() {
-        if (gamepad2.dpad_down) {
-            EncoderA.setPower(-1);
-            EncoderB.setPower(1);
-        } else if (gamepad2.dpad_up) {
-            EncoderA.setPower(1);
-            EncoderB.setPower(-1);
-        } else {
-            EncoderA.setPower(0);
-            EncoderB.setPower(0);
+        C1 = 3;
+        C2 = 0;
+        MaxVelocity = 1;
+        ErrorBand = 3;
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                LiftAndDiffLoop();
+                Climb();
+            }
         }
     }
+
+    /**
+     * Describe this function...
+     */
     private void LiftAndDiffSetup() {
         Diff2.setDirection(Servo.Direction.REVERSE);
         Outtake.setPosition(0);
